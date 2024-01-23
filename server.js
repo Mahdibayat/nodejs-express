81f437b1ef97335a993f9042f2c02ce2e1d5e4cc
mahdibayat <mahdibayat72@hotmail.com>
2024-01-23 22:36:02 +0330

    initial author route

--- server.js
+++ server.js
@@ -2,31 +2,30 @@ if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 } 
 
+// APP ============
 const express = require('express');
 const expressLayout = require('express-ejs-layouts')
 const bodyParser = require('body-parser');
-
-// ROUTES
-const indexRoutes = require('./routes/index')
-const authorsRoutes = require('./routes/authors')
-
 const app = express();
-
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayout);
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
+// DATA BASE ===========
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection
 db.on('error', console.error)
 db.on('open', () => console.log("connect to mongoose"))
 
-// ROUTES
+// ROUTES =============
+const indexRoutes = require('./routes/index')
+const authorsRoutes = require('./routes/authors')
 app.use('/', indexRoutes);
 app.use('/authors', authorsRoutes);
 
+// LISTENER ==========
 app.listen(process.env.PORT || 3000);
